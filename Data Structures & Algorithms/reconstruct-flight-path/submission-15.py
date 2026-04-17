d9a30a02d9c0448b89b8b9d633b2b813bb0ec011
Vincent T. <73805050+VTiS15@users.noreply.github.com>
2026-04-17 22:18:49 +0800

    Add: reconstruct-flight-path - submission-15

---
+++ Data Structures & Algorithms/reconstruct-flight-path/submission-15.py
@@ -0,0 +1,26 @@
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        """This problem is equivalent to finding the lexicographically smallest Eulerian path.
+        
+        Procedure:
+            1. Create adjacency heap, treating each ticket as an edge.
+            2. Use Hierholzer's algorithm to find the required flight path.
+
+        """
+        adj = collections.defaultdict(list)
+        for ticket in tickets:
+            heapq.heappush(adj[ticket[0]], ticket[1])
+
+        res = []
+        path = ["JFK"]  # Stack
+
+        while path:
+            airport = path[-1]
+            if adj[airport]:
+                path.append(heapq.heappop(adj[airport]))
+            else:
+                res.append(path.pop())
+
+        res.reverse()
+        return res
+        
